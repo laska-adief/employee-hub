759d11f1ccd96a421acadc722d9d302ff82a1cda
laska-adief <laskaadief.la@gmail.com>
2024-06-01 06:48:10 +0700

    [Employee Hub] create guard for routing

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (authService.isAuthenticated()) {
+    router.navigate(['/employee/list']);
+    return false;
+  }
+  return true;
+};
+
+export const authNotAuthorizedGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  if (!authService.isAuthenticated()) {
+    router.navigate(['/login']);
+    return false;
+  }
+  return true;
+};
